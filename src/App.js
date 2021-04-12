568446efdf38fe69f9309b43a25ecc4205e9c80c
Rinna <missrinna@outlook.com>
2021-04-12 03:37:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Navbar from './components/Navbar/index';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
-import Events from './pages/events';
-import AnnualReport from './pages/annual';
-import Teams from './pages/team';
 import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
 
